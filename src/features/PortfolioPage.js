17f49cc786aca71a953ad02111ad2c0e33c9430a
Craig Kelleher <kelleher.craig@gmail.com>
2023-04-05 13:15:33 -0700

    Added a Stock Marketplace

--- src/features/PortfolioPage.js
+++ src/features/PortfolioPage.js
@@ -1,18 +1,21 @@
 import React from "react";
 import PortfolioTable from './PortfolioTable';
+import Marketplace from "../Components/Marketplace";
 import '../PortfolioPage.css';
 
 function PortfolioPage() {
     const portfolio = [
-        { symbol: 'AAPL', numberOfShares: 10, amountInvested: 1500},
-        { symbol: 'MSFT', numberOfShares: 5, amountInvested: 2500},
-        { symbol: 'TSLA', numberOfShares: 50, amountInvested: 20000}
+        { symbol: 'AAPL', name: "Apple Inc.", numberOfShares: 10, amountInvested: 1500},
+        { symbol: 'MSFT', name: "Microsoft Corporation", numberOfShares: 5, amountInvested: 2500},
+        { symbol: 'TSLA', name: "Telsa Corporation", numberOfShares: 50, amountInvested: 20000}
     ];
 
     return (
         <div>
+            <h2 className="section-header"> My Portfolio </h2>
             <PortfolioTable portfolio={portfolio} />
-            <h2> My Portfolio </h2>
+            <h2 className="section-header"> Marketplace</h2>
+            <Marketplace Marketplace={Marketplace} />
         </div>
     )
 }
