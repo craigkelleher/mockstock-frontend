d1751434ea64ea9b03019ec88d51be0ff414a323
Craig Kelleher <kelleher.craig@gmail.com>
2023-04-10 09:08:23 -0700

    Allow the user to remove stocks from their portfolio

--- src/features/PortfolioTable.js
+++ src/features/PortfolioTable.js
@@ -3,36 +3,30 @@ import '../PortfolioPage.css';
 import axios from "axios";
 
 function PortfolioTable({ portfolio, userId, stockPrice, fetchPortfolio, formatNumber }) {
-
     const [sharesToBuyOrSell, setSharesToBuyOrSell] = useState({});
-
     function handleBuyShares(stockSymbol) {
-
-        if (window.confirm(`Do you really want to purchase ${sharesToBuyOrSell[stockSymbol]} shares of ${stockSymbol}?`)) {
+        const shares = sharesToBuyOrSell[stockSymbol] ?? 0; // set shares to 0 if undefined or null
+        if (window.confirm(`Do you really want to purchase ${shares} shares of ${stockSymbol}?`)) {
             const transaction = {
                 stockSymbol: stockSymbol,
                 transactionType: "buy",
-                quantity: sharesToBuyOrSell[stockSymbol]
+                quantity: shares
             };
-            
-            // axios.post(`http://localhost:8080/api/user/${userId}/transactions`,
-            // transaction)
-            //     .then(() => {
-            //         handleShareChange(0, stockSymbol)
-            //     })
-
-            axios.post(`http://springbootmockstockaws-env.eba-m9mpenp5.us-west-1.elasticbeanstalk.com/api/user/${userId}/transactions`,
-            transaction)
+            axios.post(`http://springbootmockstockaws-env.eba-m9mpenp5.us-west-1.elasticbeanstalk.com/api/user/${userId}/transactions`, transaction)
                 .then(() => {
                     fetchPortfolio();
-                    handleShareChange(null, stockSymbol)
+                    handleShareChange(null, stockSymbol);
                 });
         }
     };
 
     function handleSellShares(stockSymbol) {
-
-        if (window.confirm(`Do you really want to sell ${sharesToBuyOrSell[stockSymbol]} shares of ${stockSymbol}?`)) {
+        const stock = portfolio.find(stock => stock.stockSymbol === stockSymbol);
+        const shares = sharesToBuyOrSell[stockSymbol] ?? 0;
+        if(stock.quantity === 0){
+            handleRemoveClick(stockSymbol);
+        }
+        else if(window.confirm(`Do you really want to sell ${sharesToBuyOrSell[stockSymbol]} shares of ${stockSymbol}?`)) {
             const transaction = {
                 stockSymbol: stockSymbol,
                 transactionType: "sell",
@@ -59,6 +53,15 @@ function PortfolioTable({ portfolio, userId, stockPrice, fetchPortfolio, formatN
             ...prevState,
             [stockSymbol]: parseInt(value) || null
         }));
+    }
+
+    function handleRemoveClick(stockSymbol){
+        if(window.confirm('Do you really want to remove ${stockSymbol} from your portfolio?')){
+            axios.delete(`http://springbootmockstockaws-env.eba-m9mpenp5.us-west-1.elasticbeanstalk.com/api/user/${userId}/portfolio/${stockSymbol}`)
+                .then(() => {
+                    fetchPortfolio();
+                })
+        }
     }
 
     return (
